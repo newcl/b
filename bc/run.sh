6d6d583fb394bdfccd3856e9b6c9a735351aab58
Liang Chen <chenliang.info@gmail.com>
2020-01-16 09:55:54 -0800

    Pre write/read channel abstraction

--- bc/run.sh
+++ bc/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-for i in {1..100}
+for i in {1..5}
 do
   subPort=$((9000 + i))
-  java -jar target/bc-1.0-SNAPSHOT.jar --client.pubPort=40125 --client.subPort=$subPort &
+  nohup java -jar target/bc-1.0-SNAPSHOT.jar --client.pubPort=40125 --client.subPort=$subPort &
 done
---
+++ bs/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+nohup java -jar target/bs-1.0-SNAPSHOT.jar &
